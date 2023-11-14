4272604a4f65cedcecb613075ba29dfe2300f464
zzuzzu0205 <zkakcnd@naver.com>
2023-11-14 22:11:06 +0900

    [Feat]학생 로그인

--- api/models.py
+++ api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Student(models.Model):
     name = models.CharField(max_length=255)
+    user = models.OneToOneField(User, on_delete=models.CASCADE , null=True)
     department = models.CharField(max_length=255)
     student_id = models.IntegerField(unique=True)
     year = models.IntegerField()
@@ -16,3 +18,4 @@ class Professor(models.Model):
 
     def __str__(self):
         return self.name
+
